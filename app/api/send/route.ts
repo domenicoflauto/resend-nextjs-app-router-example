768867728bec387e2e97d8dc6d3650c230044377
Domenico Flauto <18469530+domenicoflauto@users.noreply.github.com>
2024-10-03 15:44:56 +0100

    Adds to and from emails as env variables

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -7,8 +7,8 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST() {
   try {
     const { data, error } = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['delivered@resend.dev'],
+      from: `Test <${process.env.FROM_EMAIL}>`,
+      to: [process.env.TO_EMAIL],
       subject: "Hello world",
       react: EmailTemplate({ firstName: "John" }) as React.ReactElement,
     });
